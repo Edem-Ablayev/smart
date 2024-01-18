1aee6e5b413bdea8701023914f9ab6eee9ca6a8e
Edem Ablayev <eablaev3@gmail.com>
2024-01-18 15:10:40 +0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int onAccount = 469; // на счету
+        int refill = 1200; // сумма пополнения
+
+        int bonus = refill / 100; // бонусы
+        int sum; // итоговая сумма
+
+        if (refill > 1000) {
+            sum = onAccount + refill + bonus; // если сумма пополнения больше 1000, то начисляем бонусы
+        } else {
+            sum = onAccount + refill; // иначе - не начисляем
+        }
+
+        System.out.println(sum);
+    }
+}
