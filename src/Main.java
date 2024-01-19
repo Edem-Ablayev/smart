476eb6564e8869f862e6cded4bc92443c0fd4747
Edem Ablayev <eablaev3@gmail.com>
2024-01-19 18:43:26 +0300

    correct bonus

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        int onAccount = 469; // на счету
-        int refill = 1200; // сумма пополнения
+        int onAccount = 100; // на счету
+        int refill = 300; // сумма пополнения
 
         int bonus = refill / 100; // бонусы
         int sum; // итоговая сумма
 
         if (refill > 1000) {
             sum = onAccount + refill + bonus; // если сумма пополнения больше 1000, то начисляем бонусы
+            System.out.println("Бонус: " + bonus);
         } else {
             sum = onAccount + refill; // иначе - не начисляем
+            System.out.println("Бонус: 0");
         }
-        System.out.println("Бонус: " + bonus);
+
         System.out.println("Итоговая сумма: " + sum);
     }
 }
